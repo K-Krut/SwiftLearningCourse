865c295e38bf81eb31527516a9e7611ac9df798e
K-Krut <katiakrut448@gmail.com>
2024-07-28 22:25:22 +0300

    Initial Commit

---
+++ AppDict/AppDictApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppDictApp.swift
+//  AppDict
+//
+//  Created by Katia Krut on 28.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppDictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
