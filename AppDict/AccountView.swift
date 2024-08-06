82389c8bab444d20d34a504c7f8cdbcedc82b146
K-Krut <katiakrut448@gmail.com>
2024-08-06 14:31:27 +0300

    adding ui to account view and testing modifiers

--- AppDict/AccountView.swift
+++ AppDict/AccountView.swift
@@ -9,7 +9,45 @@ import SwiftUI
 
 struct AccountView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            
+            List {
+                VStack(spacing: 8) {
+                    Image(systemName: "person.crop.circle.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .font(.system(size: 32))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .blue.opacity(0.3))
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
+                        .background(Image(systemName: "hexagon")
+                            .symbolVariant(.fill)
+                            .foregroundColor(.blue)
+                            .font(.system(size: 200))
+                            .offset(x: -50, y: -100)
+                    )
+                    Text("Testing kdf")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/.weight(.semibold))
+                    HStack {
+                        Image(systemName: "location").imageScale(.large)
+                        Text("TEST 2")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                 Section {
+                     Label("Settings", systemImage: "gear")
+                     Label("Billing", systemImage: "creditcard")
+                     Label("Help", systemImage: "questionmark")
+                         .imageScale(.small)
+                }
+                 .listRowSeparatorTint(.blue)
+                 .listRowSeparator(.hidden)
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Account")
+        }
     }
 }
 
