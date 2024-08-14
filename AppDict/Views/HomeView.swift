917af09a0b3fcbd9caba61900f70190cabdba16f
K-Krut <katiakrut448@gmail.com>
2024-08-14 20:22:29 +0300

    courses card ui added

--- AppDict/Views/HomeView.swift
+++ AppDict/Views/HomeView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct HomeView: View {
     @State var hasScrolled = false
+    @Namespace var namespace
+    @State var show = false
+    
     
     var body: some View {
         ZStack {
@@ -19,7 +22,21 @@ struct HomeView: View {
                 
                 featured
                 
-                Color.clear.frame(height: 1000)
+                Text("Courses".uppercased())
+                    .font(.footnote.weight(.semibold))
+                    .foregroundColor(.secondary)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 20)
+                
+                if !show {
+                    CourseItem(namespace: namespace, show: $show)
+                        .onTapGesture {
+                            withAnimation(.spring(response: 0.6, dampingFraction: 0.8)) {
+                                show.toggle()
+                            }
+                        }
+                }
+                
             }
             .coordinateSpace(name: "scroll")
             .safeAreaInset(edge: .top, content: {
@@ -28,6 +45,9 @@ struct HomeView: View {
             .overlay(
                 NavigationBar(title: "Featured", hasScroll: $hasScrolled)
             )
+            if show {
+                CourseView(namespace: namespace, show: $show)
+            }
         }
     }
     
